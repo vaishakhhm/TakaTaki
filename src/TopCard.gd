e3759b7bc092a3e039714e976f4d32e2e00b1f26
vaishakhhm <vaishakh_hm@yahoo.in>
2020-05-16 14:56:34 +0530

    Added background and sprites for the cards, including basic initial animation to move the card to the center of the screen.

---
+++ src/TopCard.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 9000
+const ACCELERATION = 10000
+const FRICTION = 10000
+
+var velocity = Vector2.ZERO
+var card_click = false
+
+func _ready():
+	set_physics_process(false)
+	pass
+
+
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	var movement_vector = move_the_card(delta)
+	
+
+func _on_Card_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton):
+		if(Input.is_mouse_button_pressed(BUTTON_LEFT)):
+			print("Got Click event.")
+			set_physics_process(true)
+
+
+func _on_Card_mouse_entered():
+	print("Mouse entered the object.")
+
+
+func _on_Card_mouse_exited():
+	print("Mouse left the object.")
+	
+func move_the_card(var delta):
+	var distance = Vector2(500, 350) - position
+	velocity = distance
+	
+	velocity = velocity.normalized()
+	velocity = velocity.move_toward(velocity * MAX_SPEED, ACCELERATION * delta)	
+	velocity = move_and_slide(velocity)
+	
+	return velocity
