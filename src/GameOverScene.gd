f484ebbab77f7b79085b5b099ecb398617b58499
vaishakhhm <vaishakh_hm@yahoo.in>
2020-05-28 13:48:23 +0530

    Added code to quit and play again.

---
+++ src/GameOverScene.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_PlayAgain_pressed():
+	get_tree().change_scene("res://Scenes/Background.tscn")
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
